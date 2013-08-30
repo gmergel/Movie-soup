6dcae1c21eca77bb85b9f1505d5d6d178e7a2793
Germano <gmergel@gmail.com>
2013-08-30 18:17:15 -0300

    Update batch.php

--- batch.php
+++ batch.php
@@ -8,42 +8,68 @@ $data = new Spreadsheet_Excel_Reader("test.xls");
 $sheet = 0; //informal
 
 	$column = 1;
-	$row = 1;
-	$extracted = array();
-
-	while($data->val($row,$column,$sheet) != ''){
-		$extracted[$data->val($row,$column,$sheet)] = array();
-		$column++;
-	}
-
-	$headers = array_keys($extracted);
-	$row++;
-	$column = 1;
-
+	$row = 2;
+	
 	while($data->val($row,$column,$sheet) != ''){
 
 		while($data->val($row,$column,$sheet) != ''){
 			
-			if(!count($extracted[$headers[$column-1]])) $extracted[$headers[$column-1]] = array();
-			array_push($extracted[$headers[$column-1]], $data->val($row,$column,$sheet));
-			$row++;
+			//if(!count($extracted[$headers[$column-1]])) $extracted[$headers[$column-1]] = array();
+			//array_push($extracted[$headers[$column-1]], $data->val($row,$column,$sheet));
+			
+			switch($column){
+				case 1:
+					$inumber = $data->val($row,$column,$sheet);
+					if(!count($extracted[$inumber])){
+						$extracted[$inumber] = array();
+						$extracted[$inumber]['dates'] = array();
+						$extracted[$inumber]['midturns'] = array();
+					}
+				break;				
+				
+				case 2:
+					$dayoff = $data->val($row,$column,$sheet);
+					array_push($extracted[$inumber]['dates'],$dayoff);
+				break;
+
+				case 3:
+					$midturn = (strtolower($data->val($row,$column,$sheet)) == 'yes')? 1 : 0;
+					array_push($extracted[$inumber]['midturns'],$midturn);
+				break;
+			}
+
+			$column++;
 
 		}
-		$row = 2;
-		$column++;
+		$column = 1;
+		$row++;
 
 	}
 
-	print_r($extracted);
-/*
+	//print_r($extracted);
+	$counter = 1;
 
 	foreach($extracted as $key => $value){
 
-		echo "insert into table daysoff () values ()" ;
+		$tbiinumber = $key;
+		$tbidates = $value['dates'];
+		$tbimidturns = $value['midturns'];
 
+		echo "<br><hr>".$counter."<hr>insert into requests (person_idperson, tag, approved, sent, date_sent, date_approved) values ((Select idperson from person where LOWER(inumber) = LOWER('".$tbiinumber."')),'fix',1,1,NOW(),NOW());<br>";
+
+		$sqldates = "insert into daysoff (requests_idrequests, person_idperson, date, midturn) values ";
+
+		foreach($tbidates as $idx => $date){
+			if($idx != 0) $sqldates .= ',';
+			$sqldates .= "((SELECT max( idrequests ) FROM requests), (Select idperson from person where LOWER(inumber) = LOWER('".$tbiinumber."')), '".$date."', ".$tbimidturns[$idx].")" ;
+		}
+		$sqldates .= ';';
+		echo $sqldates;
+
+		$counter++;
 	}
 
-*/	
+	
 //}
 
 ?>
